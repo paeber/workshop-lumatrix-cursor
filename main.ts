7270498c72e67fabf3e0b1aefbedaf15dd611155
Pascal Eberhard <35179162+paeber@users.noreply.github.com>
2025-05-14 09:45:19 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,21 @@
+lumaMatrix.joystickChangedThread(function (direction) {
+    if (lumaMatrix.compareJoystick(direction, lumaMatrix.eJoystickDirection.Center)) {
+    	
+    } else if (lumaMatrix.compareJoystick(direction, lumaMatrix.eJoystickDirection.Up)) {
+    	
+    } else if (lumaMatrix.compareJoystick(direction, lumaMatrix.eJoystickDirection.Down)) {
+    	
+    } else if (lumaMatrix.compareJoystick(direction, lumaMatrix.eJoystickDirection.Left)) {
+    	
+    } else if (lumaMatrix.compareJoystick(direction, lumaMatrix.eJoystickDirection.Right)) {
+    	
+    } else {
+    	
+    }
+    lumaMatrix.setOnePixel(cursor_x, cursor_y, 0xffffff)
+})
+let cursor_y = 0
+let cursor_x = 0
+lumaMatrix.initializeMatrix(127)
+cursor_x = 0
+cursor_y = 0
